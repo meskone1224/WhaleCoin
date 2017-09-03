e0e556ea192f86b7ec23c24816b4dbbe58778872
Rhett Creighton <rhett@heyrhett.com>
2017-09-03 15:49:37 -0500

    change genesis extradata

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -93,4 +93,5 @@ var (
 	MinimumDifficulty      = big.NewInt(131072) // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(20)     // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 	SlowStart 			   = big.NewInt(1000)
+
 )
